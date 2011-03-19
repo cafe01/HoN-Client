8a932454a4c2c9b55aed8b38b7e08dea0382d231
Carlos Fernando Avila Gratz <cafe@q1software.com>
2011-03-19 15:11:30 -0300

    UserUpdate fully decoded.
    Final attributes:
    - account_id
    - state
    	- online
    	- in_lobby
    	- in_game
    - flags
    	- is_moderator
    	- is_founder
    	- is_prepurchased
    - clan
    - symbol
    - color
    - icon

--- lib/HoN/Client/Chat/Packet/UserUpdate.pm
+++ lib/HoN/Client/Chat/Packet/UserUpdate.pm
@@ -12,10 +12,24 @@ class_has 'decode_id'   => ( is => 'ro', isa => 'Int', default => 0x0c00 );
 
 has 'event_name'  => ( is => 'rw', isa => 'Str', default => 'user_update' );
 
-has 'clan'   => ( is => 'rw', isa => 'Str', default => '' );
-has 'symbol' => ( is => 'rw', isa => 'Str', default => '' );
-has 'color'  => ( is => 'rw', isa => 'Str', default => '' );
-has 'icon'   => ( is => 'rw', isa => 'Str', default => '' );
+has 'account_id'   => ( is => 'rw', isa => 'Int', default => 0 );
+has 'state'   => ( is => 'rw', isa => 'Int' );
+has 'flags'   => ( is => 'rw', isa => 'Int' );
+
+has 'online'    => ( is => 'rw', isa => 'Bool', default => 0);
+has 'in_game'   => ( is => 'rw', isa => 'Bool', default => 0);
+has 'in_lobby'  => ( is => 'rw', isa => 'Bool', default => 0);
+
+has 'is_moderator'    => ( is => 'rw', isa => 'Bool', default => 0);
+has 'is_founder'      => ( is => 'rw', isa => 'Bool', default => 0);
+has 'is_prepurchased' => ( is => 'rw', isa => 'Bool', default => 0);
+
+
+
+has 'clan'   => ( is => 'rw', isa => 'Str' );
+has 'symbol' => ( is => 'rw', isa => 'Str' );
+has 'color'  => ( is => 'rw', isa => 'Str' );
+has 'icon'   => ( is => 'rw', isa => 'Str' );
 
 
 # add C code
@@ -91,7 +105,41 @@ sub _decode_packet {
     my $unpacked = $self->unpack($self->name, $data);
     
     # populate attributes
-    $self->$_($unpacked->{$_}) for (qw/ clan symbol color icon/);
+    $self->$_($unpacked->{$_}) for (qw/ account_id state flags clan symbol color icon/);
+    
+    # decode state
+    my $state = $unpacked->{state};
+#    Offline: 0x00 
+#    Online: 0x03 
+#    In Lobby: 0x04 
+#    In Game: 0x05 
+    my %state_mask = (
+        online   => 0x03,
+        in_lobby => 0x04, 
+        in_game  => 0x05,
+    );
+    
+    for (qw/ online in_lobby in_game/) {
+        $self->$_(1) if (($state & $state_mask{$_}) == $state_mask{$_});
+    }
+    
+    
+    # decode flags
+    my $flags = $unpacked->{flags};
+    #    None: 0x00 
+    #    Moderator: 0x01 
+    #    Founder: 0x02 
+    #    Prepurchased:0x40 
+    my %flag_mask = (
+        moderator    => 0x01, 
+        founder      => 0x02,
+        prepurchased => 0x40 
+    );
+    
+    for (keys %flag_mask) {
+        my $attr = "is_$_";
+        $self->$attr(1) if (($flags & $flag_mask{$_}) == $flag_mask{$_});
+    }
 }
 
 
--- t/05-packetfactory.t
+++ t/05-packetfactory.t
@@ -60,15 +60,28 @@ is($pkt_join->packed, pack( 'H*', '1e0043616d6261646100'), 'JoinChannel - right 
 
 # BE3C03000300000000000076656E657A75656C61007768697465004861707079204661636500
 
-my $pkt_uu = $factory->decode_packet(0x0c00, pack( 'H*', 'BE3C03000300000000000076656E657A75656C61007768697465004861707079204661636500'));
+my $pkt_uu = $factory->decode_packet(0x0c00, pack( 'H*', 'BE3C03000341000000000076656E657A75656C61007768697465004861707079204661636500'));
 
 isa_ok($pkt_uu, 'HoN::Client::Chat::Packet::UserUpdate', 'UserUpdate - thing returned by decode_packet()');
 
+# main attributes
+is($pkt_uu->account_id, 0x00033CBE, 'UserUpdate - account_id');
+is($pkt_uu->state, 0x03, 'UserUpdate - state');
+is($pkt_uu->flags, 0x41, 'UserUpdate - flags');
 is($pkt_uu->clan, '', 'UserUpdate - clan');
 is($pkt_uu->symbol, 'venezuela', 'UserUpdate - symbol');
 is($pkt_uu->color, 'white', 'UserUpdate - color');
 is($pkt_uu->icon, 'Happy Face', 'UserUpdate - icon');
 
+# decoded state
+is($pkt_uu->online, 1, 'UserUpdate - state - online');
+is($pkt_uu->in_game, 0, 'UserUpdate - state - in_game');
+is($pkt_uu->in_lobby, 0, 'UserUpdate - state - in_lobby');
+
+# decoded flags
+is($pkt_uu->is_moderator, 1, 'UserUpdate - flag - is_moderator');
+is($pkt_uu->is_founder, 0, 'UserUpdate - flag - is_founder');
+is($pkt_uu->is_prepurchased, 1, 'UserUpdate - flag - is_prepurchased');
 
 
 
